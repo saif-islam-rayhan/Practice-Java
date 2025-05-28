85fe12a9f6ae99b94cc5919f3ee1a4de61ba6eba
Saif Islam Rayhan <134764307+saif-islam-rayhan@users.noreply.github.com>
2025-05-28 22:03:34 +0600

    Create Convert_ArrayToList.java

---
+++ Convert_ArrayToList.java
@@ -0,0 +1,14 @@
+package conver.arr.lis.arr;
+import java.util.*;
+import java.util.Arrays;
+
+public class ArrayToList {
+
+    public static void main(String[] args){
+        String[]  fruitsArray={"Mango","banana","Apple"};
+        List<String>frutsList= Arrays.asList(fruitsArray);
+
+        System.out.println(frutsList);
+
+    }
+}
